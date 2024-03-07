98b66eabf526a71b5fd69fdc568c8ccc6704c525
blondel.sita <blondel.sita@10.150.20.200>
2024-03-07 20:23:06 +0100

    Merge origin/devStart into devStart
    Conflicts:
    	Base/src/main/java/MesVariables.java

--- Base/src/main/java/org/blc/training/base/QuelEstTonAGE.java
+++ Base/src/main/java/org/blc/training/base/QuelEstTonAGE.java
@@ -6,5 +6,12 @@ package org.blc.training.base;
  * @author blondel.sita
  */
 public class QuelEstTonAGE {
+
     
-}
+    public static void main (String [] args){
+            
+         System.out.println("tu as" + args[0]);
+         
+         
+}    
+}    
